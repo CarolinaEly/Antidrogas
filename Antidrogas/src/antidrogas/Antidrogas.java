bef9d4ad4811e2ed275cfcc7e7d6f75ec4fe4c40
Villa <carolinaelizabeth_27@hotmail.com>
2017-07-04 07:42:49 -0500

    se realizo el cuadro de dialogo

--- Antidrogas/src/antidrogas/Antidrogas.java
+++ Antidrogas/src/antidrogas/Antidrogas.java
@@ -5,6 +5,8 @@
  */
 package antidrogas;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Fausto
@@ -16,6 +18,7 @@ public class Antidrogas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JOptionPane.showMessageDialog(null,"Campos Vacios");
     }
     
 }
