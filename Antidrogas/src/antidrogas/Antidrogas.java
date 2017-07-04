a436375f01e4d80d136c9d2221c67aaa60d32ea5
CarolinaEly <carolinaelizabeth_27@hotmail.com>
2017-07-04 07:46:23 -0500

    Update Antidrogas.java

--- Antidrogas/src/antidrogas/Antidrogas.java
+++ Antidrogas/src/antidrogas/Antidrogas.java
@@ -19,6 +19,8 @@ public class Antidrogas {
     public static void main(String[] args) {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null,"Campos Vacios");
+         JOptionPane.showMessageDialog(null,"1");
+         JOptionPane.showMessageDialog(null,"2");
     }
     
 }
